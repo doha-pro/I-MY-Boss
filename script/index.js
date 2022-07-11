e5733019c0883f802b34d9dd0e05ef1c5ca9dc61
Doha Kassem <doha.kassem97@gmail.com>
2022-07-12 01:15:43 +0200

    ask for help

--- script/index.js
+++ script/index.js
@@ -21,20 +21,93 @@ function setLanguage(lang) {
 
 
 function setPage(lang) {
-
+    let domainsSubMenu;
+    let hostingSubMenu;
     $.ajax({
         type: "GET",
         url: "../languages/" + lang + ".json",
         success: function(response) {
             pageText = response.homePage;
-            document.getElementById("domains").innerHTML = document.getElementById("domainsLabel").innerHTML = pageText.navbar.domains
-            document.getElementById("hosting").innerHTML = document.getElementById("hostingLabel").innerHTML = pageText.navbar.hosting;
-            document.getElementById("security").innerHTML = document.getElementById("securityLabel").innerHTML = pageText.navbar.security;
-            document.getElementById("development").innerHTML = document.getElementById("developmentLabel").innerHTML = pageText.navbar.development;
-            document.getElementById("marketing").innerHTML = document.getElementById("marketingLabel").innerHTML = pageText.navbar.marketing;
-
+            domainsSubMenu = response.homePage.navbar.domainsMenu;
+            hostingSubMenu = response.homePage.navbar.hostingMenu;
+            console.log("domainsMenu= " + domainsSubMenu)
+            getEle("domains").innerHTML = getEle("domainsLabel").innerHTML = pageText.navbar.domains
+            getEle("hosting").innerHTML = getEle("hostingLabel").innerHTML = pageText.navbar.hosting;
+            getEle("security").innerHTML = getEle("securityLabel").innerHTML = pageText.navbar.security;
+            getEle("development").innerHTML = getEle("developmentLabel").innerHTML = pageText.navbar.development;
+            getEle("marketing").innerHTML = getEle("marketingLabel").innerHTML = pageText.navbar.marketing;
+            getEle("domianRegisterationSubMenu").innerHTML = domainsSubMenu.domianRegisteration.title;
+            getEle("transferOfDomainsSubMenu").innerHTML = domainsSubMenu.transferOfDomains.title;
+            getEle("serachWhoIsSubMenu").innerHTML = domainsSubMenu.serachWhoIs.title;
+            getEle("domianPolicySubMenu").innerHTML = domainsSubMenu.domianPolicy.title;
+            // // Hosting sub Menu
+            getEle("wordPressHostingSubMenu").innerHTML = hostingSubMenu.wordPressHosting.title;
+            getEle("sharedHostingSubMenu").innerHTML = hostingSubMenu.sharedWebHosting.title;
+            getEle("businessWebHostingSubMenu").innerHTML = hostingSubMenu.businessWebHosting.title;
+            getEle("emailHostingSubMenu").innerHTML = hostingSubMenu.emailHosting.title;
+            getEle("domianSearchHeader").innerHTML = response.homePage.domainSearchBar.header
         }
     })
 
 
 }
+
+function getEle(id) {
+    let ele = document.getElementById(id);
+    return ele
+}
+
+/**
+ * 
+// Development menu
+
+
+
+// Conception Web
+
+Conception Web
+Recevez un devis sous 48 h!
+
+    DEMANDER UN DEVIS
+
+Développement Web
+Recevez un devis sous 48 h!
+
+    DEMANDER UN DEVIS
+
+
+Développement d 'API
+Recevez un devis sous 48 h!
+
+    DEMANDER UN DEVIS
+
+
+
+
+
+// 
+
+SEO
+Plans Start at $4 .50 per month!
+
+    LEARN MORE
+
+
+SEA
+Plans Start at $7 .50 per month!
+
+    LEARN MORE
+
+
+SMM
+Plans Start at $9 .50 per month!
+
+    LEARN MORE
+
+
+// Content Writing
+Rédaction de Contenu
+Plans Start at $11 .50 per month!
+
+    LEARN MORE
+ */
